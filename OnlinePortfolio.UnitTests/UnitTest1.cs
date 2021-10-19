6ca05a7ae7a3df33609efbd3d66dfea44b872a6b
Alibek <effuone@github.com>
2021-10-19 19:27:04 +0600

    added three projects

---
+++ OnlinePortfolio.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OnlinePortfolio.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
